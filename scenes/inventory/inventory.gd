be38cf7a0f90876ca889ba8e9f044fefa6a1a3e1
AlhemyD <alhemyd@mail.ru>
2024-12-04 22:49:36 +0800

    Starting creating textures. Created testing bulb (by me)

--- scenes/inventory/inventory.gd
+++ scenes/inventory/inventory.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 
+
 func _input(event):
 	if event.is_action_pressed("inventory") and not get_tree().get_root().get_node("Node").get_node("DialogueBox").d_active:
 		$InvPanel.toggle()
