b27f254003aebaf3b7827f92d2f3dc713027f27d
AlhemyD <alhemyd@mail.ru>
2025-03-11 00:39:00 +0800

    camera limits corrected. restricted fullscrean mode, only window gameplay. DialogueBox now at the bottom always.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,9 +1,22 @@
 extends CharacterBody2D
 @export var speed=250
+@export var limit_left = -10000000
+@export var limit_top = -10000000
+@export var limit_right = 10000000
+@export var limit_bottom = 10000000
+
+
+
 var screen_size
 
 func _ready():
 	screen_size = Vector2(pow(2,20),pow(2,20))
+	var cam=$Camera2D
+	cam.limit_left = limit_left
+	cam.limit_top=limit_top
+	cam.limit_bottom=limit_bottom
+	cam.limit_right=limit_right
+	
 func _physics_process(delta):
 	if get_tree().get_root().get_node("Node").get_node("DialogueBox"):
 		if get_tree().get_root().get_node("Node").get_node("DialogueBox").d_active:
@@ -34,3 +47,7 @@ func _physics_process(delta):
 	elif velocity.y != 0 && velocity.y<0:
 		$AnimatedSprite2D.animation="up"
 	move_and_slide()
+
+
+
+	
